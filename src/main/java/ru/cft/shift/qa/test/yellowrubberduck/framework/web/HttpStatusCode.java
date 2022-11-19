98b9753234c7a07f8f3df8528fba9171eab780ef
SLAVA <mr.glazyrinslava@mail.ru>
2022-11-19 14:01:38 +0700

    Поправил гит игнор

--- src/main/java/ru/cft/shift/qa/test/yellowrubberduck/framework/web/HttpStatusCode.java
+++ src/main/java/ru/cft/shift/qa/test/yellowrubberduck/framework/web/HttpStatusCode.java
@@ -3,7 +3,6 @@ package ru.cft.shift.qa.test.yellowrubberduck.framework.web;
 public class HttpStatusCode {
     public static final int OK = 200;
     public static final int NOT_FOUND = 404;
-
    public static final int  BAD_REQUEST =400;
     public static final int INTERNAL_SERVER_ERROR = 500;
 }
